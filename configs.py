1aa9fd99cd564ca6f7d5d176cc2a07105748197a
Rajps44 <singhayush88805@gmail.com>
2024-08-11 22:57:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "modijiurl.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "3a316a64da231058d60e832717d6c32da304d12d")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "2016390875"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Inder:Joyus231@cluster0.as05z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001535343753")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002157674551"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
