422e6927df43774ce5adefe2701182b56bcdcc12
Rajps44 <singhayush88805@gmail.com>
2024-08-12 00:32:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "2016390875"))
+  API_ID = int(os.environ.get("API_ID", "23929647"))
   API_HASH = os.environ.get("API_HASH", "b9afa697042d998a758e407b84c86daf")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "6541237957:AAGo2ooB5uGlbutXK_I5G0P5GOBXfrSo3_o")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Chadarmod_bot")
@@ -16,7 +16,7 @@ class Config(object):
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
